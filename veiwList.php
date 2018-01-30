e20a195d3f37f47ab981a3d0580aecd17f514b9b
Minsoo Kang (BetaMan) <33021438+skymins04@users.noreply.github.com>
2018-01-31 00:19:33 +0900

    180131 DB 리스트 기능 추가

--- veiwList.php
+++ veiwList.php
@@ -1,18 +1,41 @@
 <?php
-$db_conn = mysqli_connect('localhost','','','');
-if(mysqli_connect_errno()) die('DB 서버에 연결할 수 없습니다. '.mysqli_connect_error());
+$list = array();
+if($_GET["list"] == "TB") {
+	$DBname = $_GET["DB"];
+	if($DBname == NULL) die('DB 명을 입력하세요');
 
-$q = mysqli_query($db_conn,'show tables');
-$tables = array();
-$i = 0;
-while($row = mysqli_fetch_array($q)) {
-	$tables[$i] = $row[0];
-	$i += 1;
-}
+	$db_conn = mysqli_connect('localhost','arduino','arduino',$DBname);
+	if(mysqli_connect_errno()) die('DB 서버에 연결할 수 없습니다. '.mysqli_connect_error());
 
-for($i=0;$i<count($tables);$i++) {
-	echo $tables[$i];
-	if($i != count($tables)-1) echo ',';
+	$q = mysqli_query($db_conn,'show tables');
+	$i = 0;
+	while($row = mysqli_fetch_array($q)) {
+		$list[$i] = $row[0];
+		$i += 1;
+	}
+
+	for($i=0;$i<count($list);$i++) {
+		echo $list[$i];
+		if($i != count($list)-1) echo ',';
+	}
+	mysqli_close($db_conn);
 }
-mysqli_close($db_conn);
+else {
+	$db_conn = mysqli_connect('localhost','arduino','arduino');
+        if(mysqli_connect_errno()) die('DB 서버에 연결할 수 없습니다. '.mysqli_connect_error());
+
+        $q = mysqli_query($db_conn,'show databases');
+        $list = array();
+        $i = 0;
+        while($row = mysqli_fetch_array($q)) {
+                $list[$i] = $row[0];
+                $i += 1;
+        }
+
+        for($i=0;$i<count($list);$i++) {
+                echo $list[$i];
+                if($i != count($list)-1) echo ',';
+        }
+        mysqli_close($db_conn);
+}
 ?>
